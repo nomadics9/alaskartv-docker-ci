7f55ed002c6bc93ff3341ad357d1e93aee0c303b
nomad <sager@alaskar.dev>
2025-11-06 20:22:24 +0300

    jellyfin updated customization 10.11 + dns fix| Test build10

--- alaskarfin/customization/update-assets.sh
+++ alaskarfin/customization/update-assets.sh
@@ -4,7 +4,7 @@ WEB_ROOT="/jellyfin/jellyfin-web"
 # your custom assets (place them in the same dir for simplicity)
 CUSTOM_BANNER="$WEB_ROOT/assets/img/banner-light.png"
 CUSTOM_ICON="$WEB_ROOT/assets/icon-transparent.png"
-CUSTOM_FAV="$WEB_ROOT/faveicon.ico/favicon.ico"
+CUSTOM_FAV="$WEB_ROOT/favicon/favicon.ico"
 
 # overwrite banner-light.*.png
 find "$WEB_ROOT" -type f -name "banner-light.*.png" -exec cp -f "$CUSTOM_BANNER" {} \;
